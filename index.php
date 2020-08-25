4099271c6a0ddf1e304aa37a3b6afb6947615a1f
vanivilasam <65148965+vanivilasam@users.noreply.github.com>
2020-08-25 15:19:28 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html >
+<?php
+  
+  if (isset($_POST[Upload])){
+
+  	$file ['file']
+  	print(file);
+
+  }
+  	
+
+
+
+
+?>
+<html>
+<head>Harikesh</head>
+
+<body>
+	<form action="?" method="POST" enctype="multipart/form-data" >
+		<label>Uploadfiles</label>
+       <p> <input type="file" name="file"> </p>
+       <p><input type="submit" name="upload" value="Upload Images"></p>
+	</form>
+</body>
+</html>
